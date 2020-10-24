216f475eebd57f154b333a1d6a7316ea78da2630
benvredenburg <vre19001@byui.edu>
2020-10-24 13:43:30 -0600

    10/24

--- Lesson-5/js/preston.js
+++ Lesson-5/js/preston.js
@@ -3,8 +3,32 @@ function responsive() {
 }
 
 window.addEventListener('load', (event)=>{
-    const lu = document.querySelector('#lastupdated');
-    lu.textContent = document.lastModified;
+    const cd = document.querySelector('#lastupdated');
+    let day = [
+        'Sunday', 
+        'Monday', 
+        'Tuesday', 
+        'Wednesday', 
+        'Thursday', 
+        'Friday', 
+        'Saturday'
+    ]
+    let month = [
+        'January', 
+        'February', 
+        'March', 
+        'April', 
+        'May', 
+        'June', 
+        'July', 
+        'August', 
+        'September', 
+        'October', 
+        'Novemeber', 
+        'December'
+    ]
+    cd.textContent = day[new Date().getDay()] + ', ' + new Date().getDate() + ' ' + month[new Date().getMonth()] + ' ' + new Date().getFullYear();
+
 
     const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
