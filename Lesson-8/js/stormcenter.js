1dfbb8453ced918b74572a8716c641c04137eb75
benvredenburg <vre19001@byui.edu>
2020-11-02 23:42:33 -0700

    11/02

---
+++ Lesson-8/js/stormcenter.js
@@ -0,0 +1,45 @@
+function responsive() {
+    document.getElementsByClassName("nav-children")[0].classList.toggle("expand");
+}
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+window.addEventListener('load', (event) => {
+    const cd = document.querySelector('#lastupdated');
+    let day = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    let month = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+    cd.textContent = day[new Date().getDay()] + ', ' + new Date().getDate() + ' ' + month[new Date().getMonth()] + ' ' + new Date().getFullYear();
+
+
+    const cry = document.querySelector('#copyrightyear');
+    cry.textContent = new Date().getFullYear();
+
+    const bd = document.querySelector('#banner');
+    today = new Date().getDay();
+    if (today != 5) {
+        bd.style.display = 'none';
+    }
+})
