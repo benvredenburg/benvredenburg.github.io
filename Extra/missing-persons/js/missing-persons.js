841e6b3537320ae67727bed268f3e709874e3c44
benvredenburg <vre19001@byui.edu>
2020-11-16 14:57:21 -0700

    11/16

--- Extra/missing-persons/js/missing-persons.js
+++ Extra/missing-persons/js/missing-persons.js
@@ -1,5 +1,5 @@
 async function getperson() {
-    const requestURL = 'person.json';
+    const requestURL = 'https://www.ahfx.com/person.php';
 
     const response = await fetch(requestURL)
         .then(function(response) {
@@ -17,20 +17,35 @@ async function getperson() {
     let p2 = document.createElement('p');
     let p3 = document.createElement('p');
     let p4 = document.createElement('p');
+    let p6 = document.createElement('p');
+    let p7 = document.createElement('p');
+    let p8 = document.createElement('p');
+    let p9 = document.createElement('p');
+    let p10 = document.createElement('p');
 
-    h2.textContent = "Full name: " + response.person.personal.name + response.person.personal.last_name;
-    p5.textContent = "Password: " + response.person.online_info.password;
-    p1.textContent = "Email: " + response.person.online_info.email;
-    p2.textContent = "Eye Color: " + response.person.personal.eye_color;
-    p3.textContent = "Resident of: " + response.person.personal.city + response.person.personal.country;
-    p4.textContent = "IP Address: " + response.person.online_info.ip_address;
+    h2.textContent = "Full name: " + response.person.personal.name + " " + response.person.personal.last_name;
+    p5.textContent = "Password: ";
+    p6.textContent = response.person.online_info.password;
+    p1.textContent = "Email: ";
+    p7.textContent = response.person.online_info.email;
+    p2.textContent = "Eye Color: ";
+    p8.textContent = response.person.personal.eye_color;
+    p3.textContent = "Resident of: ";
+    p9.textContent = response.person.personal.city + ", " + response.person.personal.country;
+    p4.textContent = "IP Address: ";
+    p10.textContent = response.person.online_info.ip_address;
 
     info.appendChild(h2);
-    info.appendChild(p);
     info.appendChild(p1);
-    info.appendChild(p2);
-    info.appendChild(p3);
+    info.appendChild(p7);
+    info.appendChild(p5);
+    info.appendChild(p6);
     info.appendChild(p4);
+    info.appendChild(p10);
+    info.appendChild(p2);
+    info.appendChild(p8);
+    info.appendChild(p3);
+    info.appendChild(p9);
 
     document.querySelector('#information').appendChild(info);
 
---
+++ Lesson-10/js/weatherapi.js
@@ -0,0 +1,48 @@
+//* Request JSON from OpenWeatherMap
+
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=3282517940169cc2ec517af5aa5f93e2';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    //* Store data in variables
+
+    var currentCondition = weatherData.weather[0].main;
+    var highTemp = weatherData.main.temp_max;
+    var humidity = weatherData.main.humidity;
+    var mainTemp = weatherData.main.temp;
+    var windSpeed = weatherData.wind.speed;
+    var imageConditions = "http://openweathermap.org/img/w/";
+
+    //* Write data into page using element IDs and variables
+
+    document.getElementById('current-weather').innerHTML = currentCondition;
+    document.getElementById('temp-high').innerHTML = highTemp;
+    document.getElementById('humid').innerHTML = humidity;
+    document.getElementById('windTemp').innerHTML = mainTemp;
+    document.getElementById('windSpeed').innerHTML = windSpeed;
+    document.getElementById('windChill').innerHTML = getWindChill();
+
+    document.getElementById("conditions-icon").setAttribute("src", imageConditions + weatherData.weather[0].icon + ".png");
+    document.getElementById("conditions-icon").setAttribute("alt", weatherData.weather[0].description);
+}
+
+//* Calculate wind chill
+
+function getWindChill() {
+    var tempF = parseInt(document.getElementById('windTemp').innerHTML);
+    var speed = parseInt(document.getElementById('windSpeed').innerHTML);
+    result = windChill(tempF, speed);
+    return result;
+}
+
+function windChill(tempF, speed) {
+    var windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+    var soCold = windChillFactor.toFixed(2);
+
+    return soCold;
+}
