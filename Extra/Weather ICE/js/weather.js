1551098fae7ce9c63cdd99d06279ca303fb0dcdd
benvredenburg <vre19001@byui.edu>
2020-11-20 15:49:25 -0700

    11/20

--- Extra/Weather ICE/js/weather.js
+++ Extra/Weather ICE/js/weather.js
@@ -9,35 +9,21 @@ weatherRequest.onload = function() {
 
 
     let currentCondition = weatherData.weather[0].main;
-    let temp = Math.round(weatherData.main.temp, 0);
-    let humidity = Math.round(weatherData.main.humidity, 0);
-    let windspeed = weatherData.wind.speed;
+    let temp = Math.round(weatherData.main.temp, 0) + '°';
+
+
     let imageConditions = 'https://openweathermap.org/weather-conditions';
 
 
     document.getElementById('current-weather').innerHTML = currentCondition;
     document.getElementById('temp').innerHTML = temp;
-    document.getElementById('humid').innerHTML = humidity;
-    document.getElementById('windspeed').innerHTML = windspeed;
-    document.getElementById('windChill').innerHTML = getWindChill();
+
 
     document.getElementById('conditions-icon').setAttribute('src', imageConditions + weatherData.weather[0].icon + '.png');
     document.getElementById('conditions-icon').setAttribute('alt', weatherData.weather[0].description);
 }
 
-function getWindChill() {
-    let tempF = parseInt(document.getElementById('temp').innerHTML);
-    let speed = parseInt(document.getElementById('windspeed').innerHTML);
-    result = windChill(tempF, speed);
-    return result;
-}
 
-function windChill(tempF, speed) {
-    let windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
-    let soCold = windChillFactor.toFixed(0);
-
-    return soCold;
-}
 
 let forecastRequest = new XMLHttpRequest();
 let forecastApiURLstring = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=3282517940169cc2ec517af5aa5f93e2';
@@ -133,21 +119,7 @@ forecastRequest.onload = function() {
     document.getElementById('img-4').setAttribute('src', imageWeather + dayFour.weather[0].icon + '.png');
     document.getElementById('img-5').setAttribute('src', imageWeather + dayFive.weather[0].icon + '.png');
 
-    document.getElementById('img-1').setAttribute('alt', dayOne.weather[0].description);
-    document.getElementById('img-2').setAttribute('alt', dayOne.weather[0].description);
-    document.getElementById('img-3').setAttribute('alt', dayOne.weather[0].description);
-    document.getElementById('img-4').setAttribute('alt', dayOne.weather[0].description);
-    document.getElementById('img-5').setAttribute('alt', dayOne.weather[0].description);
 
-    document.getElementById('condition-1').innerHTML = dayOne.weather[0].main;
-    document.getElementById('condition-2').innerHTML = dayTwo.weather[0].main;
-    document.getElementById('condition-3').innerHTML = dayThree.weather[0].main;
-    document.getElementById('condition-4').innerHTML = dayFour.weather[0].main;
-    document.getElementById('condition-5').innerHTML = dayFive.weather[0].main;
 
-    document.getElementById('low-1').innerHTML = Math.round(dayOne.main.temp_min, 0) + '&deg;';
-    document.getElementById('low-2').innerHTML = Math.round(dayTwo.main.temp_min, 0) + '&deg;';
-    document.getElementById('low-3').innerHTML = Math.round(dayThree.main.temp_min, 0) + '&deg;';
-    document.getElementById('low-4').innerHTML = Math.round(dayFour.main.temp_min, 0) + '&deg;';
-    document.getElementById('low-5').innerHTML = Math.round(dayFive.main.temp_min, 0) + '&deg;';
+
 }
